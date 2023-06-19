734111fb70abc28cbdb19b5ea89d62e92a80ad20
Tung Dinh <tung.dinh@waystar.com>
2023-06-19 18:18:06 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const connection = mongoose.connection;
 connection.once('open', () => {
 console.log('MongoDB database connection established successfully');
 });
-
+console.log("hello world")
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
   });
